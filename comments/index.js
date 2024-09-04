9e346036fb062cee8ea23984df76c79ed3ecfb45
thesystemsmith <abdulrasheed.techy@gmail.com>
2024-09-04 23:55:06 +0530

    emit events from post and comment to event bus

--- comments/index.js
+++ comments/index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const {randomBytes} = require('crypto')
 const cors = require('cors')
+const axios = require('axios')
+
 
 const app = express()
 app.use(bodyParser.json())
@@ -15,7 +17,7 @@ app.get('/posts/:id/comments', (req, res)=>{
     res.send(commentsByPostId[req.params.id] || [])
 })
 
-app.post('/posts/:id/comments', (req, res)=>{
+app.post('/posts/:id/comments', async (req, res)=>{
     const commentId = randomBytes(4).toString('hex')
     const {content} = req.body
 
@@ -28,6 +30,16 @@ app.post('/posts/:id/comments', (req, res)=>{
     })
     // update memory
     commentsByPostId[req.params.id] = comments
+
+    //emit event to event bus
+    await axios.post('http://localhost:4005/events', {
+        type: 'CommentCreated',
+        data: {
+            id:commentId,
+            content,
+            postId: req.params.id
+        }
+    })
 
     res.status(201).send(comments)
 })
