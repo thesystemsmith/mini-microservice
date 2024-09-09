71507de30972b0c0cca6ac53e5e281be91f63d8c
thesystemsmith <abdulrasheed.techy@gmail.com>
2024-09-09 22:34:06 +0530

    listening to events on query service from the event bus

--- comments/index.js
+++ comments/index.js
@@ -59,17 +59,19 @@ app.post('/events', async (req, res) => {
             return comment.id === id
         })
         comment.status= status
+
+        await axios.post('http://localhost:4005/events', {
+            type: 'CommentUpdated',
+            data: {
+                id,
+                status,
+                postId,
+                content
+            }
+        })
     }
 
-    await axios.post('http://localhost:4005/events', {
-        type: 'CommentUpdated',
-        data: {
-            id,
-            status,
-            postId,
-            content
-        }
-    })
+    
     res.send({})
 })
 
--- query/index.js
+++ query/index.js
@@ -28,6 +28,18 @@ app.post('/events', (req, res) => {
         post.comments.push({id, content, status})
     }
 
+    if(type == 'CommentUpdated'){
+        const {postId, id, status, content} = data
+
+        const post = posts[postId]
+        const comment = post.comments.find(comment => {
+            return comment.id == id
+        })
+
+        comment.status = status
+        comment.content = content
+    }
+
     console.log(type, 'event recieved', posts)
     res.send({})
 })
