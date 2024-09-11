ac40d7ce30593c82bd3612dda189457a4ddc9848
thesystemsmith <abdulrasheed.techy@gmail.com>
2024-09-11 12:46:53 +0530

    rendering only the moderated comments

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -15,7 +15,19 @@ export default ({comments}) => {
 
     //display comments
     const renderedComments = comments.map(comment => {
-        return <li key={comment.id}>{comment.content}</li>
+        let content
+
+        if(comment.status == 'approved'){
+            content = comment.content
+        }
+        if(comment.status == 'pending'){
+            content='this comment is awaiting moderation'
+        }
+        if(comment.status == 'rejected'){
+            content = 'this comment is rejected'
+        }
+
+        return <li key={comment.id}>{content}</li>
     })
 
     return <ul>{renderedComments}</ul>
