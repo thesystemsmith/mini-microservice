8c83411437f18b4b898dde4fc7bd730e67406d88
thesystemsmith <abdulrasheed.techy@gmail.com>
2024-09-28 04:09:55 +0530

    [cluster ip]: routing other services to the event bus cluster ip address

--- query/index.js
+++ query/index.js
@@ -53,7 +53,7 @@ app.listen(4002, async () => {
     console.log('listening on port', 4002)
 
     // get events from event bus eventStore
-    const res = await axios.get('http://localhost:4005/events')
+    const res = await axios.get('http://event-bus-srv:4005/events')
     //log events
     for(let event of res.data){
         console.log('processing event: ', event.type)
